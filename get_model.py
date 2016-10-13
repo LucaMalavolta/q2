6cdc56a0d03125f8413b4ed7cbc28731d3758f29
Luca Malavolta <luca.malavolta@unipd.it>
2016-10-13 12:14:53 -0400

    get_model.py now accept temporary directory for head.tmp, body.tmp and tail.tmp

--- get_model.py
+++ get_model.py
@@ -4,7 +4,7 @@ import modatm
 from config import *
 from tools import read_csv
 
-def get_model(teff, logg, vt, feh, grid,  file_name='model.in'):
+def get_model(teff, logg, vt, feh, grid,  file_name='model.in', temporary_dir=''):
     """Creates a model atmosphere file for MOOG from the model_atmosphere
     attribute of a Star object.
     """
@@ -12,14 +12,14 @@ def get_model(teff, logg, vt, feh, grid,  file_name='model.in'):
     model_atmosphere_grid = grid
 
 
-    with open('head.tmp', 'w') as f:
+    with open(temporary_dir+'head.tmp', 'w') as f:
         f.write('KURUCZ\n')
         f.write('TEFF='+str(teff)+',LOGG='+str(logg)+
                 ',[FE/H]='+str(feh)+','+model_atmosphere_grid+'\n')
         nd = len(model_atmosphere['T'])
         f.write('ND=       '+str(nd)+'\n')
 
-    with open('body.tmp', 'w') as f:
+    with open(temporary_dir+'body.tmp', 'w') as f:
         for idx in range(nd):
             f.write("{0:.8E} {1:.1F} {2:.3E} {3:.3E} {4:.3E}\n".format(\
                     model_atmosphere['RHOX'][idx],\
@@ -29,7 +29,7 @@ def get_model(teff, logg, vt, feh, grid,  file_name='model.in'):
                     model_atmosphere['ABROSS'][idx])
                    )
 
-    with open('tail.tmp', 'w') as f:
+    with open(temporary_dir+'tail.tmp', 'w') as f:
         f.write('%5.2F\n' %vt)
         if model_atmosphere_grid != 'marcs':
             path = os.path.join(MODATM_PATH, 'kurucz')
@@ -69,7 +69,7 @@ def get_model(teff, logg, vt, feh, grid,  file_name='model.in'):
         f.write('  10108.0 60808.0\n')
         f.write('  6.1     7.1     8.1   12.1  22.1  26.1\n')
 
-    file_list = ['head.tmp', 'body.tmp', 'tail.tmp']
+    file_list = [temporary_dir+'head.tmp', temporary_dir+'body.tmp', temporary_dir+'tail.tmp']
     with open(file_name, 'w') as outfile:
         for one_file in file_list:
             with open(one_file) as infile:
